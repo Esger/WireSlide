ebe5a137b2fc172f7787d07e6cfb088f834b3d7a
Esger <esgerj@gmail.com>
2023-12-19 14:11:16 +0100

    Bord vullen

--- src/resources/elements/board/board.js
+++ src/resources/elements/board/board.js
@@ -1,9 +1,21 @@
-import {bindable} from 'aurelia-framework';
+import { bindable } from 'aurelia-framework';
 
 export class Board {
-  @bindable value;
+    @bindable value;
 
-  valueChanged(newValue, oldValue) {
-    //
-  }
+    constructor() {
+        this.boardSize = 4;
+        this.blocks = [];
+    }
+    attached() {
+        this.fillBoard();
+    }
+
+    fillBoard() {
+        for (let i = 0; i < this.boardSize; i++) {
+            for (let j = 0; j < this.boardSize; j++) {
+                this.blocks.push({ x: i, y: j });
+            }
+        }
+    }
 }
