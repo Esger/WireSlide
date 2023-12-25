da01ae6c124419fb85b7a8104ea4d5be5c2bcd8a
Esger <esgerj@gmail.com>
2023-12-25 03:08:47 +0100

    scoreboard

--- src/resources/elements/scoreboard/scoreboard.js
+++ src/resources/elements/scoreboard/scoreboard.js
@@ -1,9 +1,20 @@
-import {bindable} from 'aurelia-framework';
+import { bindable, inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
+@inject(EventAggregator)
 export class Scoreboard {
-  @bindable value;
 
-  valueChanged(newValue, oldValue) {
-    //
-  }
+    constructor(eventAggregator) {
+        this._eventAggregator = eventAggregator;
+        this.moves = 0;
+        this.wins = 0;
+    }
+
+    attached() {
+        this._eventAggregator.subscribe('restart', _ => {
+            this.moves = 0;
+        });
+        this._eventAggregator.subscribe('toEmpty', _ => this.moves++);
+        this._eventAggregator.subscribe('ledGrounded', _ => this.wins++);
+    }
 }
