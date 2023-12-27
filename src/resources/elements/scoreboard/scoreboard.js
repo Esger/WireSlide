c2b3b7ed572f3c95b3d2b59e5f14f7b5243e87d6
Esger <esgerj@gmail.com>
2023-12-27 19:52:51 +0100

    game state feedback: win / loose

--- src/resources/elements/scoreboard/scoreboard.js
+++ src/resources/elements/scoreboard/scoreboard.js
@@ -8,14 +8,17 @@ export class Scoreboard {
         this._eventAggregator = eventAggregator;
         this.moves = 0;
         this.wins = 0;
+        this.gameStates = ['', 'connected', 'shortcircuit'];
+        this.gameState = 0;
     }
 
     attached() {
-        this._restartSubscription = this._eventAggregator.subscribe('restart', _ => this.moves = 0);
-        this._restartLevelSubscription = this._eventAggregator.subscribe('restartlevel', _ => this.moves = 0);
-        this._nextSubscription = this._eventAggregator.subscribe('next', _ => this.moves = 0);
+        this._restartSubscription = this._eventAggregator.subscribe('restart', _ => this._reset());
+        this._restartLevelSubscription = this._eventAggregator.subscribe('restartlevel', _ => this._reset());
+        this._nextSubscription = this._eventAggregator.subscribe('next', _ => this._reset());
         this._toEmptySubscription = this._eventAggregator.subscribe('toEmpty', _ => this.moves++);
-        this._ledGroundedSubscription = this._eventAggregator.subscribe('ledGrounded', _ => this.wins++);
+        this._ledGroundedSubscription = this._eventAggregator.subscribe('ledGrounded', _ => this._win());
+        this._shortCircuitSubscription = this._eventAggregator.subscribe('shortCircuit', _ => this._loose());
     }
 
     detached() {
@@ -25,4 +28,19 @@ export class Scoreboard {
         this._ledGroundedSubscription.dispose();
         this._restartLevelSubscription.dispose();
     }
+
+    _loose() {
+        this.gameState = 2;
+    }
+
+    _win() {
+        this.wins++;
+        this.gameState = 1;
+    }
+
+    _reset() {
+        this.moves = 0;
+        this.gameState = 0;
+    }
+
 }
