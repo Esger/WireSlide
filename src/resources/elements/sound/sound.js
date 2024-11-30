6e9a0844aeb3b369d761148d981ba12e634ccfb0
Esger <esgerj@gmail.com>
2024-11-30 17:22:22 +0100

    Bump sound unmoving

--- src/resources/elements/sound/sound.js
+++ src/resources/elements/sound/sound.js
@@ -7,24 +7,34 @@ export class SoundCustomElement {
     constructor(element, eventAggregator) {
         this._element = element;
         this._eventAggregator = eventAggregator;
+        this.sounds = [
+            'slide',
+            'bump'
+        ];
         this.addBumpSubscription();
     }
 
     attached() {
-        this._audioElement = this._element.querySelector('.slide');
-        this._audioElement.addEventListener('load', _ => this.addBumpSubscription(), { once: true });
+        this.sounds.forEach(sound => {
+            this._element.querySelector('.' + sound)?.addEventListener('load', _ => this.addBumpSubscription(), { once: true });
+        });
     }
 
     addBumpSubscription = _ => {
-        this._bumpSubscription = this._eventAggregator.subscribeOnce('bump', _ => this._playSound());
+        this._bumpSubscription?.dispose();
+        this._slideSubscription?.dispose();
+        this._bumpSubscription = this._eventAggregator.subscribeOnce('bumpSound', _ => this._playSound('bump'));
+        this._slideSubscription = this._eventAggregator.subscribeOnce('slideSound', _ => this._playSound('slide'));
     }
 
-    _playSound() {
-        this._audioElement.play();
-        setTimeout(this.addBumpSubscription, 500);
+    _playSound(name) {
+        this._element.querySelector('.' + name)?.play();
+        clearTimeout(this.timeOutHandle);
+        this.timeOutHandle = setTimeout(this.addBumpSubscription, 500);
     }
 
     detached() {
         this._bumpSubscription.dispose();
+        this._slideSubscription.dispose();
     }
 }
